c165a93dce78d5d51e8c102b27e8449a0577ab43
Lauren Zou <zou.lauren@gmail.com>
2012-10-01 01:41:43 -0400

    Added user profile. Still need to prevent users from seeing/editing each other's accounts.

--- app/helpers/bottles_helper.rb
+++ app/helpers/bottles_helper.rb
@@ -1,24 +1,3 @@
 module BottlesHelper
- def get_price(price)
-  price_string = price.to_s
-  dollar = price_string[0,price_string.length-2]
-  cents = price_string[price_string.length-2, price_string.length]
-  if dollar.length == 0
-    dollar = "0"
-  end
-  price_string = "$".concat(dollar).concat(".").concat(cents)
-  return price_string
- end
-
- def get_image_path(bottle)
-  image_path = (simplify_string(bottle.company.name)).concat("_").concat(simplify_string(bottle.name)).concat(".png")
-  image_path = image_path.downcase
-  return image_path
- end
-
- def simplify_string(string)
-  string = string.gsub("'", "")
-  string = string.gsub(" ", "_")
-  return string
- end
+ 
 end
--- app/models/bottle.rb
+++ app/models/bottle.rb
@@ -5,10 +5,28 @@ class Bottle
   key :name, String, :required => true
   key :color, String
   key :price, Integer
- 
-  key :have_ids, Array
-  many :haves, :class_name => 'User', :in => :have_ids
-
+  
   belongs_to :company
+  
+  def price_string
+    price_string = price.to_s
+    dollar = price_string[0,price_string.length-2]
+    cents = price_string[price_string.length-2, price_string.length]
+    if dollar.length == 0
+      dollar = "0"
+    end
+    price_string = "$".concat(dollar).concat(".").concat(cents)
+    return price_string
+  end
+  
+  def image_path
+    return "#{simplify_string(company.name)}_#{simplify_string(name)}.png".downcase
+  end
+  
+  def simplify_string(string)
+    string = string.gsub("'", "")
+    string = string.gsub(" ", "_")
+    return string
+  end
 
 end
